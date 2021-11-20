6e0322f8c09ba6ab5f3e6702ccdf69732a33c37d
Erik Hess <me@erikhess.com>
2021-11-20 12:51:47 -0600

    Updating proximtiy_simpletest example with basic configuration information

--- examples/apds9960_proximity_simpletest.py
+++ examples/apds9960_proximity_simpletest.py
@@ -2,19 +2,32 @@
 # SPDX-License-Identifier: MIT
 
 import board
-import digitalio
+from digitalio import DigitalInOut, Pull
 from adafruit_apds9960.apds9960 import APDS9960
 
 i2c = board.I2C()
-int_pin = digitalio.DigitalInOut(board.D5)
+
+# Replace "D5" with the GPIO connected to the sensor's interrupt pin
+int_pin = DigitalInOut(board.D5)
+int_pin.switch_to_input(pull=Pull.UP)
 apds = APDS9960(i2c)
 
-apds.enable_proximity = True
-apds.proximity_interrupt_threshold = (0, 175)
+# LED settings can be tuned to quickly increase/decrease range and sensitivity
+# This configures the IR LED for 8 pulses, 8us each, 100mA @ x1 boost
+apds.proximity_led_config = (7, 1, 0, 0)
+
+# Interrupt thresholds can be tuned to decide when the interrupt pin gets asserted
+# This configures the interrupt with 0 far threshold, 175 near threshold, 5 cycle persistence
+apds.proximity_interrupt_threshold = (0, 175, 5)
+
+# This enables assertion of the interrupt pin (pulling it to ground) when the thresholds are met
 apds.enable_proximity_interrupt = True
 
+# This starts running continously running proximity engine loops on the sensor
+apds.enable_proximity = True
+
 while True:
-    # print the proximity reading when the interrupt pin goes low
+    # Print the proximity reading when the interrupt pin goes low
     if not int_pin.value:
         print(apds.proximity)
 
